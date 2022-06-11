c6f3cba529564b26de66a388d3353891e3dec0e9
Kasper Skårhøj <kasper@skaarhoj.com>
2022-06-11 20:12:55 +0200

    Fix to secure opaque images for BMP (StreamDeck Mini)

--- image.go
+++ image.go
@@ -47,6 +47,17 @@ func getImageForButton(img image.Image, btnFormat string) ([]byte, error) {
 	case "JPEG":
 		jpeg.Encode(&b, img, &jpeg.Options{Quality: 100})
 	case "BMP":
+		// Opaque images are necessary, otherwise you won't get anything but black. So, lets remove any alpha channel without regard to pre-multiplied alpha (could be done smarter, but we assume there is no significant transparency in the image in the first place)
+		if !img.(*image.RGBA).Opaque() {
+			bounds := img.(*image.RGBA).Bounds()
+			for x := bounds.Min.X; x < bounds.Max.X; x++ {
+				for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+					r, g, b, _ := img.(*image.RGBA).At(x, y).RGBA()
+					img.(*image.RGBA).Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), 255})
+				}
+			}
+		}
+
 		bmp.Encode(&b, img)
 	default:
 		return nil, errors.New("Unknown button image format: " + btnFormat)
